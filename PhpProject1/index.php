b23d215e8c403f71e4c2205180f5f96d9e5c71ff
118350613 <118350613@BISB53VL.bis.ad.ucc.ie>
2019-02-21 14:11:42 +0000

    sixth Commit -- Finished main Page

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -7,10 +7,18 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title> PHP</title>
+        <title>Main Page</title>
+        <link rel="stylesheet" type="text/css" href="Main_Page.css"
+              
+
     </head>
     <body> 
-      
+        <ul>
+            <li><a class="active" href="CV.HTML.html">CV</a></li>
+            <li><a href="Interest.html"</a>Interests</li>
+            <li><a href="Ebusiness1.php">Ebusiness</a></li>
+        
+        
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -21,7 +29,9 @@ and open the template in the editor.
 
         <p> </p>
         <?php
-        echo 'hello world';
+    
+      
+       
         ?>
     </body>
 </html>

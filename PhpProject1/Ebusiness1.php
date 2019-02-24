d71ad475c9a36ceb89651503bc30473c8ca1beb4
118350613 <118350613@BISB53VL.bis.ad.ucc.ie>
2019-02-24 13:29:48 +0000

    Finished Calculator 24/02

--- PhpProject1/Ebusiness1.php
+++ PhpProject1/Ebusiness1.php
@@ -1,84 +1,128 @@
-
-<html>
+<?php
+session_start();
+$totalValue="";
+$_SESSION('txtTotal') = $totalValue;
+?>
+<!DOCTYPE html>
+<html lang="en" xmlns="http.//www.w3.org/1999/xhtml">
 
      <head>
-         <title>Calculator</title>
-            
-<script language ="javascript" type="text/javascript">
-                 <!--assigning values to the radio buttons-->
-function subTotal() {
- //Enter in Prices Here
- var x = 5;
- var y = 10;
- var p = x + y * 12;
- var b = y * 12;
- 
-    if (document.getElementById('basicProgram').checked) {
-     //Basic package is checked
-     document.calculator.total.value = b;
-    
-        } else if (document.getElementById('proProgram').checked) {
-         // Pro package is checked
-         document.calculator.total.value = p;
-    }
-        
- }
-
-<!--Calculations for discount and total-->
-function calcDisc(){
-var subTot = document.calculator.total.value;
-var discTotal = ((subTot /100 *10))
-document.calculator.discount.value = discTotal;
-}
-
-function calcTotal(){
-var subTott = document.calculator.total.value;
-
-var discTotal = document.calculator.discount.value;
-
-var wholeTotal = (subTott - discTotal);
-
-document.calculator.wholeTotal.value = wholeTotal;
-}
-    </script> 
+         <meta charset="utf-8"/>
+         <title>Ebusiness 1</title>
+         <link rel="stylesheet" href="Ebusiness.css" type="text/css"
     </head>
-
-   
      <body>
-              <!-- Opening a HTML form -->
-         <form name="calculator">
-            
-         <!--User fills out form here -->
-        
-         <br />
-         <input type="radio" name="programType" id="basicProgram" value="Basic" /> Basic
-         <input type="radio" name="programType" id="proProgram" value="Pro" checked /> Pro 
-        
-         <!-- Here Results will be displayed -->
-        
-         <br />
-         Subtotal: <input type="text" name="total">
-         <br>
-         <input type="button" value="Subtotal" onclick="javascript:subTotal();">
-         </br>
-        
-         Discount: <input type="text" name="discount">
-         <br>
-         <input type="button" value="Discount" onclick="javascript:calcDisc();">
-         </br>
-        
-         Total: <input type="text" name="wholeTotal">
-         <br>
-         <input type="button" value="Total" onclick="javascript:calcTotal();">
-        
-        
-         </form>
-
-
-
-
+              <div class ="form">
+                  <form name="intCalc" method="post" action="Ebusiness3.php">
+                      <h1>Shop Calculator</h1>
+                      <hr/>
+                      <center>
+                          <table cellspacing="10">
+                              <tr>
+                                  <td><b>Beverage</b></td>
+                              </tr>
+                              <tr>
+                                  <td>CocaCola</td>
+                                  <td> <input type="radio" id="CocaCola" name="RadGroup" value="1"/></td>
+                              </tr>
+                              <tr>
+                                  <td>Sprite</td>
+                                  <td> <input type="radio" id="Sprite" name="RadGroup" value="1"/></td>
+                              </tr>
+                              <tr>
+                                  <td>Fanta</td>
+                                  <td> <input type="radio" id="Fanta" name="RadGroup" value="1"/></td>
+                              </tr>                            
+                          </table>
+                      </center>
+                      <br/>
+                      
+                      <center>
+                          <table cellspacing="10">
+                              <tr>
+                                  <td><b></b></td>
+                                  <td><b>Amount</b></td>
+                              </tr>
+                              <tr>
+                                  <td>sub Total</td>
+                                  <td><input type="text" id="txtSubTot" name="txtSub" readonly/></td>
+                              </tr>
+                               <tr>
+                                  <td>Discount @30%</td>
+                                  <td><input type="text" id="txtDisc" name="txtDisc" readonly/></td>
+                               </tr>
+                               <tr>
+                                  <td>VAT @ 40%</td>
+                                  <td><input type="text" id="txtVAT" name="txtVAT" readonly/></td>
+                              </tr>
+                               <tr>
+                                  <td>Total</td>
+                                  <td><input type="text" id="txtTotal" name="txtTotal" readonly/></td>
+                              </tr>
+                               
+                          </table>
+                      </center>
+                      
+                      <br/>
+                      <center>
+                          <input type="button" name="btnCalc" id="btnCalc" oneclick="calcSub()" value="Calculate"/>
+                          <input type="button" name="btnClear" id="btnClear" oneclick="AmountClear()" value="Clear"/>
+                          <input type="submit" name="btnProceed" id="btnCalc" oneclick="Proceed" value="Proceed"/>
+                      </center>
+                  </form>
+              </div>
+         
+         <script language ="javascript" type="text/javascript">
+                 <!--assigning values to the radio buttons-->
+function calcSub() {
+ //Enter in Prices Here
+ var subAmount = parseFloat(document.getElementById('txtSubTot').value);
+ var CocaCola = parseFloat(document.getElementById('CocaCola').value);
+ var Sprite = parseFloat(document.getElementById('Sprite').value);
+ var Fanta = parseFloat(document.getElementById('Fanta').value);
+ 
+ <!-- This is the if statement for the radio buttons-->
+ 
+    if (document.getElementById('CocaCola').checked) {
+        document.intCalc.txtSubTot.value = CocaCola;
+        subAmount = CocaCola;
+        calculation(subAmount);
+        }
+       
+    else if (document.getElementById('Sprite').checked) {
+        document.intCalc.txtSubTot.value = Sprite;
+        subAmount = Sprite;
+        calculation(subAmount);
+        }
+      
+     else if (document.getElementById('Sprite').checked) {
+        document.intCalc.txtSubTot.value = Fanta;
+        subAmount = Fanta;
+        calculation(subAmount);
+        }  
+   }
+   
+   <!-- Function for calculatiing the values -->
+ function calculation(parmSTotal){
+     var subTotal = parseFloat(parmSTotal);
+     var discCalc = parseFloat(subTotal * .30);
+     var vatCalc = parseFloat(subTotal * .40);
+     var total = parseFloat(subTotal - discCalc + vatCalc);
+     
+     
+     document.intCalc.txtDisc.value= discCalc;
+      document.intCalc.txtVAT.value= vatCalc;
+       document.intCalc.txtTotal.value= total;
+    }
+    
+function AmountClear(){
+      document.getElementById("txtSubTot").value="";
+       document.getElementById("txtDisc").value="";
+        document.getElementById("txtVAT").value="";
+         document.getElementById("txtTotal").value="";
+         }    
+    </script>
      </body>
-
 </html>
 
 
---
+++ PhpProject1/Ebusiness3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        
+        session_start();
+        $totalValue = $_POST['txtTotal'];
+        echo " The total value is " .$totalValue.".";
+        ?>
+          
+    </body>
+</html>
+
+
+

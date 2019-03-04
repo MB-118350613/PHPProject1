fccb7aee1ae537130d68cbafe68c232bd74b6d37
118350613 <118350613@BISB53VL.bis.ad.ucc.ie>
2019-03-04 16:33:51 +0000

    Seventh Commit Design left

--- PhpProject1/Ebusiness1.php
+++ PhpProject1/Ebusiness1.php
@@ -1,129 +1,123 @@
 <?php
 session_start();
-$totalValue="";
-$_SESSION('txtTotal') = $totalValue;
+$totalValue = "";
+$_SESSION['txtTotal'] = $totalValue;
 ?>
 <!DOCTYPE html>
-<html lang="en" xmlns="http.//www.w3.org/1999/xhtml">
-
-     <head>
-         <meta charset="utf-8"/>
-         <title>Ebusiness 1</title>
-         <link rel="stylesheet" href="Ebusiness.css" type="text/css"
+<html lang="en" xmins="http.//www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+        <title>Ebusiness </title>
+        <link rel="stylesheet" href="Ebusiness.css" type="text/css" />
     </head>
-     <body>
-              <div class ="form">
-                  <form name="intCalc" method="post" action="Ebusiness3.php">
-                      <h1>Shop Calculator</h1>
-                      <hr/>
-                      <center>
-                          <table cellspacing="10">
-                              <tr>
-                                  <td><b>Beverage</b></td>
-                              </tr>
-                              <tr>
-                                  <td>CocaCola</td>
-                                  <td> <input type="radio" id="CocaCola" name="RadGroup" value="1"/></td>
-                              </tr>
-                              <tr>
-                                  <td>Sprite</td>
-                                  <td> <input type="radio" id="Sprite" name="RadGroup" value="1"/></td>
-                              </tr>
-                              <tr>
-                                  <td>Fanta</td>
-                                  <td> <input type="radio" id="Fanta" name="RadGroup" value="1"/></td>
-                              </tr>                            
-                          </table>
-                      </center>
-                      <br/>
-                      
-                      <center>
-                          <table cellspacing="10">
-                              <tr>
-                                  <td><b></b></td>
-                                  <td><b>Amount</b></td>
-                              </tr>
-                              <tr>
-                                  <td>sub Total</td>
-                                  <td><input type="text" id="txtSubTot" name="txtSub" readonly/></td>
-                              </tr>
-                               <tr>
-                                  <td>Discount @30%</td>
-                                  <td><input type="text" id="txtDisc" name="txtDisc" readonly/></td>
-                               </tr>
-                               <tr>
-                                  <td>VAT @ 40%</td>
-                                  <td><input type="text" id="txtVAT" name="txtVAT" readonly/></td>
-                              </tr>
-                               <tr>
-                                  <td>Total</td>
-                                  <td><input type="text" id="txtTotal" name="txtTotal" readonly/></td>
-                              </tr>
-                               
-                          </table>
-                      </center>
-                      
-                      <br/>
-                      <center>
-                          <input type="button" name="btnCalc" id="btnCalc" oneclick="calcSub()" value="Calculate"/>
-                          <input type="button" name="btnClear" id="btnClear" oneclick="AmountClear()" value="Clear"/>
-                          <input type="submit" name="btnProceed" id="btnCalc" oneclick="Proceed" value="Proceed"/>
-                      </center>
-                  </form>
-              </div>
-         
-         <script language ="javascript" type="text/javascript">
-                 <!--assigning values to the radio buttons-->
-function calcSub() {
- //Enter in Prices Here
- var subAmount = parseFloat(document.getElementById('txtSubTot').value);
- var CocaCola = parseFloat(document.getElementById('CocaCola').value);
- var Sprite = parseFloat(document.getElementById('Sprite').value);
- var Fanta = parseFloat(document.getElementById('Fanta').value);
- 
- <!-- This is the if statement for the radio buttons-->
- 
-    if (document.getElementById('CocaCola').checked) {
-        document.intCalc.txtSubTot.value = CocaCola;
-        subAmount = CocaCola;
-        calculation(subAmount);
-        }
+     <a href ="Ebusiness2.php">Back</a>
+        <body> 
+        <div class="form">
+            <form name="intCalc" method="post" action="Ebusiness2.php">
+        <h1>Ebusiness</h1>   
+        <hr/>
+        <center>
+            <table cellspacing="10">
+                <tr>
+                    <td><b>Products</b></td>
+                </tr>
+                <tr>
+                    <td>Blockchain @ 1000</td>
+                    <td><input type="radio" id="CocaCola" name="rdoGroup" value="1000" /></td>
+                </tr>
+                <tr>
+                    <td>Immersive @2000</td>
+                    <td><input type="radio" id="Sprite" name="rdoGroup" value="2000" /></td>
+                </tr>
+                <tr>
+                    <td>Robots @3000</td>
+                    <td><input type="radio" id="Fanta" name="rdoGroup" value="3000" /></td>
+                </tr>
+            </table>
+        </center>
+        <br/>
+            <center>
+                 <table cellspacing="10">
+            <tr>
+                    <td><b></b></td>
+                    <td><b>Amount</b></td>
+            </tr>
+                <tr>
+                    <td>Sub Total</td>
+                    <td><input type="text" id="txtSubTot" name="txtSub" readonly /></td>
+                </tr>
+                <tr>
+                    <td>Discount @ 30%</td>
+                    <td><input type="text" id="txtDisc" name="txtDisc" readonly /></td>
+                </tr>
+                <tr>
+                    <td>+ VAT @ 40%</td>
+                    <td><input type="text" id="txtVAT" name="txtVAT" readonly /></td>
+                </tr>
+                <tr>
+                    <td>Total</td>
+                    <td><input type="text" id="txtTotal" name="txtTotal" value="" readonly/></td>
+                </tr>
+            </table>
+        </center>
        
-    else if (document.getElementById('Sprite').checked) {
-        document.intCalc.txtSubTot.value = Sprite;
-        subAmount = Sprite;
-        calculation(subAmount);
-        }
-      
-     else if (document.getElementById('Sprite').checked) {
-        document.intCalc.txtSubTot.value = Fanta;
-        subAmount = Fanta;
-        calculation(subAmount);
-        }  
-   }
-   
-   <!-- Function for calculatiing the values -->
- function calculation(parmSTotal){
-     var subTotal = parseFloat(parmSTotal);
-     var discCalc = parseFloat(subTotal * .30);
-     var vatCalc = parseFloat(subTotal * .40);
-     var total = parseFloat(subTotal - discCalc + vatCalc);
-     
-     
-     document.intCalc.txtDisc.value= discCalc;
-      document.intCalc.txtVAT.value= vatCalc;
-       document.intCalc.txtTotal.value= total;
-    }
-    
-function AmountClear(){
-      document.getElementById("txtSubTot").value="";
-       document.getElementById("txtDisc").value="";
-        document.getElementById("txtVAT").value="";
-         document.getElementById("txtTotal").value="";
-         }    
-    </script>
-     </body>
-</html>
-
-
-	
+        <br />
+            <center>
+            <input type="button" name="btnCalc" id="btnCalc" onclick="calcSub()" value="Calculate Amount"/>
+            <input type="button" name="btnClear" id="btnClear" onclick="AmountClear()" value="Clear"/>
+            <input type="submit" name="btnProceed" id="btnProceed" onclick="" value="Proceed"/>
+        </center>
+            </form>
+        </div>
+       
+            <script type="text/javascript">
+                function calcSub(){
+                    //Assigning variables to the values
+                    var subAmount = parseFloat(document.getElementById('txtSubTot').value);
+                    var CocaCola = parseFloat(document.getElementById('CocaCola').value);
+                    var Sprite = parseFloat(document.getElementById('Sprite').value);
+                    var Fanta = parseFloat(document.getElementById('Fanta').value);
+                   
+                    // if radio buttons are clicked, values are assigned
+                    if (document.getElementById('CocaCola').checked) {
+                        document.intCalc.txtSubTot.value = CocaCola;
+                        subAmount = CocaCola;
+                        calculation(subAmount);
+                       
+                    }
+                   else if (document.getElementById('Sprite').checked) {
+                        document.intCalc.txtSubTot.value = Sprite;
+                        subAmount = Sprite;
+                        calculation(subAmount);
+                       
+                    }
+                    else if (document.getElementById('Fanta').checked) {
+                        document.intCalc.txtSubTot.value = Fanta;
+                        subAmount = Fanta;
+                        calculation(subAmount);
+                    }
+                }
+               
+                //function for calculationg the values//
+                function calculation(parmsTotal) {
+                    var subTotal = parseFloat(parmsTotal);
+                    var discCalc = parseFloat(subTotal * .30);
+                    var vatCalc= parseFloat(subTotal * .40);   
+                    var total = parseFloat(subTotal - discCalc + vatCalc);
+                   
+                    //Inserting thrm intp thr correct fields//
+                    document.intCalc.txtDisc.value = discCalc;
+                    document.intCalc.txtVAT.value = vatCalc;
+                    document.intCalc.txtTotal.value = total;
+                }
+               
+                function AmountClear() {
+                    document.getElementById("txtSubTot").value="";
+                    document.getElementById("txtDisc").value="";
+                    document.getElementById("txtVAT").value="";
+                    document.getElementById("txtTotal").value="";
+                }
+                   
+            </script>
+    </body>
+    </html>
--- PhpProject1/Ebusiness3.php
+++ PhpProject1/Ebusiness3.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title> Receipt</title>
+        <link rel="stylesheet" type="text/css" href="Ebusiness2.css" />
+    </head>
     <body>
+        <a href ="Ebusiness2.php">Back</a>
+    <center>
         <?php
         
         session_start();
-        $totalValue = $_POST['txtTotal'];
-        echo " The total value is " .$totalValue.".";
+        $totalValue2 = $_POST['txtTotal'];
+        $fullNameValue =$_POST['txtName'];
+                echo "<br></br>";
+                echo "<br></br>";
+                echo " User Name:".$fullNameValue.".";
+                echo "<br></br>";
+                echo " The payment needed is  " .$totalValue2.".";
         ?>
-          
+        </center>    
     </body>
 </html>
 
